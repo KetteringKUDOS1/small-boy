ab9480f7fc079dfbab4be09934eb1004f94e95e2
Connor <awsmspwn@gmail.com>
2024-01-25 17:10:11 -0500

    tank drive

--- src/main.cpp
+++ src/main.cpp
@@ -157,8 +157,8 @@ void opcontrol() {
 
   while (true) {
 
-    //chassis.tank(); // Tank control
-     chassis.arcade_standard(ez::SPLIT); // Standard split arcade
+    chassis.tank(); // Tank control
+    //chassis.arcade_standard(ez::SPLIT); // Standard split arcade
     // chassis.arcade_standard(ez::SINGLE); // Standard single arcade
     // chassis.arcade_flipped(ez::SPLIT); // Flipped split arcade
     // chassis.arcade_flipped(ez::SINGLE); // Flipped single arcade
